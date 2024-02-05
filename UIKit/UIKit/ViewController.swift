dcd4aef29f6172974428ff00a6aabb90c3a458f3
Vitqqqqqqqq <130509045+Vitqqqqqqqq@users.noreply.github.com>
2024-02-05 15:00:01 +0300

    Commit messege

---
+++ UIKit/UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit
+//
+//  Created by Виталий Горбунов on 05.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

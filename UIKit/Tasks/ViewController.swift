22d8d38f7cd8489568bf494d74b7ed00f052b757
Vitqqqqqqqq <130509045+Vitqqqqqqqq@users.noreply.github.com>
2024-02-09 01:52:45 +0300

    Commit доделал приложение  угадай число и калькулятор

--- UIKit/Tasks/ViewController.swift
+++ UIKit/Tasks/ViewController.swift
@@ -3,29 +3,29 @@
 
 import UIKit
 
-/// Класс Вью Контроллер
+/// Класс Вью Контроллер угадай число и калькулятор
 final class ViewController: UIViewController {
-    
     // MARK: - Private Properties
+
     private let pictureBackgroundView: UIImageView = {
         let pictureBackgroundView = UIImageView()
         pictureBackgroundView.image = UIImage(named: "picture background")
         pictureBackgroundView.contentMode = .scaleAspectFill
-        pictureBackgroundView.frame = CGRect(x: 0, y: 50, width: 375, height: 775)
+        pictureBackgroundView.frame = CGRect(x: 0, y: 50, width: 380, height: 760)
         return pictureBackgroundView
     }()
-    
+
     private let nameLabel: UILabel = {
         let nameLabel = UILabel()
         nameLabel.backgroundColor = .colorWelcom
-        nameLabel.frame = CGRect(x: 0, y: 49, width: 375, height: 82)
+        nameLabel.frame = CGRect(x: 0, y: 49, width: 380, height: 82)
         nameLabel.textAlignment = .center
         nameLabel.numberOfLines = 0
         nameLabel.font = .boldSystemFont(ofSize: 30)
         nameLabel.textColor = .white
         return nameLabel
     }()
-    
+
     private lazy var additionbutton: UIButton = {
         let additionbutton = UIButton()
         additionbutton.frame = CGRect(x: 132, y: 507, width: 200, height: 200)
@@ -39,7 +39,7 @@ final class ViewController: UIViewController {
         additionbutton.addTarget(self, action: #selector(calculateResult), for: .touchUpInside)
         return additionbutton
     }()
-    
+
     private lazy var guessNumberbutton: UIButton = {
         let guessNumberbutton = UIButton()
         guessNumberbutton.frame = CGRect(x: 82, y: 301, width: 150, height: 150)
@@ -54,39 +54,55 @@ final class ViewController: UIViewController {
         guessNumberbutton.addTarget(self, action: #selector(guessNumber), for: .touchUpInside)
         return guessNumberbutton
     }()
-    
-    @objc func calculateResult() {
+
+    // MARK: - Life Cycle
+
+    override func viewDidAppear(_ animated: Bool) {
+        view.backgroundColor = .white
+        welcomeAlert()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameLabel.isHidden = true
+        setup()
+    }
+
+    // MARK: - Private Methods
+
+    @objc private func calculateResult() {
         let alert = UIAlertController(title: "Введите ваши числа", message: nil, preferredStyle: .alert)
         let alertResult = UIAlertController(title: "Ваш результат", message: "", preferredStyle: .alert)
         let actionOk = UIAlertAction(title: "Ок", style: .default)
         let cancel = UIAlertAction(title: "Отмена", style: .default)
         var finalResult = 0
-        let action = UIAlertAction(title: "Сложить", style: .cancel) { _ in
+        let action = UIAlertAction(title: "Сложить                  ", style: .default) { _ in
             if let texfield = alert.textFields {
                 let result = (Int(texfield[0].text ?? "") ?? 0) + (Int(texfield[1].text ?? "") ?? 0)
                 finalResult += result
             }
-            
-            alertResult.addAction(actionOk)
+
             alertResult.addAction(cancel)
+            alertResult.addAction(actionOk)
             alertResult.message = "\(finalResult)"
             self.present(alertResult, animated: true, completion: nil)
         }
-        
+
         alert.addTextField { texfield in
             texfield.placeholder = "Число 1"
         }
-        
+
         alert.addTextField { texfield in
             texfield.placeholder = "Число 2"
         }
-        
+
         alert.addAction(action)
         alert.addAction(cancel)
+        alert.preferredAction = action
         present(alert, animated: true)
     }
-    
-    @objc func guessNumber() {
+
+    @objc private func guessNumber() {
         let alert = UIAlertController(title: "Угадай число от 1 до 10", message: nil, preferredStyle: .alert)
         let alertRezult = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
         let cancel = UIAlertAction(title: "Отмена", style: .default)
@@ -103,34 +119,20 @@ final class ViewController: UIViewController {
                     alertRezult.message = "Это неверный ответ"
                 }
             }
-            
+
             self.present(alertRezult, animated: true, completion: nil)
         }
-        
+
         alert.addTextField { texfield in
             texfield.placeholder = "Введите число"
         }
-        
+
         alertRezult.addAction(secondAllowAction)
         alert.addAction(cancel)
         alert.addAction(allowAction)
         present(alert, animated: true)
     }
-    
-    // MARK: - Life Cycle
-    
-    override func viewDidAppear(_ animated: Bool) {
-        welcomeAlert()
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        nameLabel.isHidden = true
-        setup()
-    }
-    
-    // MARK: - Private Methods
-    
+
     private func welcomeAlert() {
         let alert = UIAlertController(title: "Пожалуйста, \n представьтесь", message: nil, preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "Готово", style: .default) { _ in
@@ -139,15 +141,15 @@ final class ViewController: UIViewController {
                 self.nameLabel.isHidden = false
             }
         }
-        
+
         alert.addTextField { UITextField in
             UITextField.placeholder = "Введите ваше имя"
         }
-        
+
         alert.addAction(cancelAction)
         present(alert, animated: true)
     }
-    
+
     private func setup() {
         view.addSubview(pictureBackgroundView)
         view.addSubview(additionbutton)
